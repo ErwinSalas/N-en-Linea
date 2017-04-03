bd32ab9e164b4cee8c43912a274192c11e941b19
ErwinSalas <erwinsalas42@gmail.com>
2017-04-02 23:38:41 -0600

    cambios en gamesession resource

--- app/Http/Controllers/GameSessionController.php
+++ app/Http/Controllers/GameSessionController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Gameboard;
 use App\Gamesession;
+use App\BoardLogic;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GameSessionController extends Controller
 {
@@ -38,6 +41,21 @@ class GameSessionController extends Controller
      */
     public function store(Request $request)
     {
+        $size=$request->input("size");
+        $board = new BoardLogic(sqrt($size),sqrt($size));
+        $gameBoard=Gameboard::create([
+            "size" => $size,
+            "rows_number" => sqrt($size),
+            "columns_numbers"=> sqrt($size),
+            "board_array" => $board
+        ]);
+        Gamesession::create([
+            "player_1_id" => Auth::user()->id,
+            "board_id" => $gameBoard->id,
+            "state" => 0
+        ]);
+
+
         //
     }
 
